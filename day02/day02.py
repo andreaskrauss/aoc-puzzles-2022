173fa26507cb6a072cd79b6af86cc20327d72ca1
Andreas Krauss <am.krauss@web.de>
2022-12-10 18:20:01 +0100

    resolve: riddles of day 2

--- day02/day02.py
+++ day02/day02.py
@@ -4,21 +4,23 @@
 # 6 = own
 # 3 = tie
 # 0 = opponent
+# Result codes: x = lose, y = draw, z = win
+# returns tupel of (points opponent, own points, required for result)
 get_result = {
     "A": {
-        "X": (3, 3),
-        "Y": (0, 6),
-        "Z": (6, 0)
+        "X": (3, 3, "Z"),
+        "Y": (0, 6, "X"),
+        "Z": (6, 0, "Y")
     },
     "B": {
-        "X": (6, 0),
-        "Y": (3, 3),
-        "Z": (0, 6)
+        "X": (6, 0, "X"),
+        "Y": (3, 3, "Y"),
+        "Z": (0, 6, "Z")
     },
     "C": {
-        "X": (0, 6),
-        "Y": (6, 0),
-        "Z": (3, 3)
+        "X": (0, 6, "Y"),
+        "Y": (6, 0, "Z"),
+        "Z": (3, 3, "X")
     }
 }
 
@@ -35,14 +37,31 @@ def get_value(action: str):
 opponents_score = 0
 own_score = 0
 
+# Challenge 1
 if __name__ == '__main__':
     with open("./input.txt") as file:
         rounds = file.readlines()
 
 for play in rounds:
     opponent, own = play.rstrip("\n").split(" ")
-    result = get_result[opponent][own]
-    (opponents_score, own_score) = (opponents_score + get_value(opponent) + result[0],
-                                    own_score + get_value(own) + result[1])
+    estimate = get_result[opponent][own]
+    (opponents_score, own_score) = (opponents_score + get_value(opponent) + estimate[0],
+                                    own_score + get_value(own) + estimate[1])
 
-print(opponents_score, " : ", own_score)
+print("Challenge 1: ", opponents_score, " : ", own_score)
+
+opponents_score = 0
+own_score = 0
+
+with open("./input.txt") as file:
+    rounds = file.readlines()
+
+
+for play in rounds:
+    opponent, own = play.rstrip("\n").split(" ")
+    estimate = get_result[opponent][own]
+    result = get_result[opponent][estimate[2]]
+    (opponents_score, own_score) = (opponents_score + get_value(opponent) + result[0],
+                                    own_score + get_value(estimate[2]) + result[1])
+
+print("Challenge 2: ", opponents_score, " : ", own_score)
