0cfbb692f66d0184941b41496f29031feaf6f44c
Andreas Krauss <am.krauss@web.de>
2022-12-28 12:19:46 +0100

    feat: resolve challenge 2 of day 5

--- 05/day05.py
+++ 05/day05.py
@@ -3,18 +3,19 @@ import re
 
 
 class Depot:
-    storage: dict = {}
 
-    def move(self, n, of, to):
-        for idx in range(n):
-            self.storage[to].append(self.storage[of].pop())
+    def __init__(self):
+        self.storage: dict = {}
+
+    def do(self, move, params):
+        move(self.storage, params[0], params[1], params[2])
 
     def set(self, item, to):
         if to not in self.storage.keys():
             self.storage[to] = []
         self.storage[to].insert(0, item)
 
-    def getTopCrates(self):
+    def get_top_crates(self):
         result = ""
         for k, v in sorted(self.storage.items()):
             if len(v) > 0:
@@ -40,20 +41,39 @@ class Depot:
         return result + header
 
 
-class DepotDispatcher:
+class CrateMover9000:
     _depot: Depot
 
     def __init__(self, depot: Depot):
-        self.depot = depot
+        self._depot = depot
 
     def dispatch(self, action: str, params):
         if action == "move":
-            self.depot.move(*params)
+            n, of, to = params
+            for idx in range(n):
+                self._depot.storage[to].append(self._depot.storage[of].pop())
+
+
+
+class CrateMover9001:
+    _depot: Depot
+
+    def __init__(self, depot: Depot):
+        self._depot = depot
+
+    def dispatch(self, action: str, params):
+        if action == "move":
+            n, of, to = params
+            self._depot.storage[to].extend(self._depot.storage[of][-n:])
+            self._depot.storage[of] = self._depot.storage[of][0:-n]
 
 
 if __name__ == '__main__':
-    d = Depot()
-    dispatcher = DepotDispatcher(d)
+    d1 = Depot()
+    mover1 = CrateMover9000(d1)
+    d2 = Depot()
+    mover2 = CrateMover9001(d2)
+
     read_config = True
 
     with open("./input.txt") as file:
@@ -68,12 +88,17 @@ if __name__ == '__main__':
                     it = stacks[i]
                     if re.match(r"\[.\]", it):
                         it = it.lstrip("[").rstrip("]")
-                        d.set(it, i+1)
+                        d1.set(it, i + 1)
+                        d2.set(it, i + 1)
                 continue
             if not read_config:
                 command = line.rstrip("\n").split(" ")
-                dispatcher.dispatch(command[0], [int(command[1]), int(command[3]), int(command[5])])
+                mover1.dispatch(command[0], [int(command[1]), int(command[3]), int(command[5])])
+                mover2.dispatch(command[0], [int(command[1]), int(command[3]), int(command[5])])
                 continue
 
-    print(d)
-    print(d.getTopCrates())
+    print(d1)
+    print(d2)
+    print(f"{os.linesep}=== Requested Outputs ===")
+    print(f"Challenge 1: {d1.get_top_crates()}")
+    print(f"Challenge 2: {d2.get_top_crates()}")
